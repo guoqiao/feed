a50ad6193fdb9b2034a58e640dbe9af0b2202b08
guoqiao <guoqiao@gmail.com>
2013-07-08 19:49:30 +0800

    mv opf out to root

--- apps/opf/management/commands/build.py
+++ apps/opf/management/commands/build.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import os
+import subprocess
 from shutil import copyfile
 from path import path
 from django.core.management.base import NoArgsCommand
 from django.contrib.auth.models import User
 #from feeds import models as m
+from django.conf import settings
 from django.template.loader import render_to_string as r2s
 
 HERE = path(os.path.abspath(__file__)).parent
 OPF = HERE.parent.parent/'templates'/'opf'
-OUT = HERE/'out'
+OUT = settings.PROJ_ROOT/'out'
 
 def build_for_user(user):
     print 'building for user:%s' % user
@@ -26,6 +28,8 @@ def build_for_user(user):
         else:
             print 'copying',rp
             copyfile(doc,r)
+    cmd = 'kindlegen %s' % (OUT/'content.opf',)
+    subprocess.call(cmd.split())
 
 
 class Command(NoArgsCommand):
