0e6c0c8b3ca1580c86aba346f155a53ad4f8b8dc
guoqiao <guoqiao@gmail.com>
2013-07-09 16:47:01 +0800

    add update and base.html

---
+++ apps/feeds/management/commands/update.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.core.management.base import NoArgsCommand
+from feeds import models as m
+
+class Command(NoArgsCommand):
+    help = "parse feeds"
+
+    def handle_noargs(self, **options):
+        for f in m.Feed.objects.all():
+            self.stdout.write('updating %s\n' % f)
+            f.update()
